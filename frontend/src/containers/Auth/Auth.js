4a2f21d98658a21ccdf1418fa12ea46046e530c2
sohagmahin <sohagmahin@gmail.com>
2022-04-28 17:35:04 +0600

    use global toaster

--- frontend/src/containers/Auth/Auth.js
+++ frontend/src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { errorToast, successToast } from "../../shared/utility";
 
 // internal imports
 import * as actions from "../../store/actions";
@@ -72,110 +73,23 @@ const Auth = () => {
       });
     }
   };
-  const successToast = (data) => (
-    <div
-      id="toast-success"
-      className="flex top-0 z-50 fixed items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
-      role="alert"
-    >
-      <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-        <svg
-          className="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </div>
-      <div className="ml-3 text-sm font-normal">{data}</div>
-      <button
-        type="button"
-        className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-        data-dismiss-target="#toast-success"
-        aria-label="Close"
-        onClick={() => setSuccessToastMsg("")}
-      >
-        <span className="sr-only">Close</span>
-        <svg
-          class="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </button>
-    </div>
-  );
-  const errorToast = (data) => (
-    <div
-      id="toast-danger"
-      className="flex top-0 fixed z-50 items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 transition duration-1000 ease-in-out"
-      role="alert"
-    >
-      <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-        <svg
-          className="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </div>
-      <div className="ml-3 text-sm font-normal">{data}</div>
-      <button
-        type="button"
-        className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-        aria-label="Close"
-        onClick={() => setErrToastMsg("")}
-      >
-        <span className="sr-only">Close</span>
-        <svg
-          className="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </button>
-    </div>
-  );
 
   return (
     <div className="hero min-h-screen bg-base-200">
       {redirectPath !== "" ? <Navigate to="/" /> : null}
       {currentAuthMode === AuthMode.SINGIN
         ? successToastMsg
-          ? successToast(successToastMsg)
+          ? successToast(successToastMsg, () => setSuccessToastMsg(""))
           : errToastMsg
-          ? errorToast(errToastMsg)
+          ? errorToast(errToastMsg, () => setErrToastMsg(""))
           : null
         : null}
 
       {currentAuthMode === AuthMode.SINGUP
         ? successToastMsg
-          ? successToast(successToastMsg)
+          ? successToast(successToastMsg, () => setSuccessToastMsg(""))
           : errToastMsg
-          ? errorToast(errToastMsg)
+          ? errorToast(errToastMsg, () => setErrToastMsg(""))
           : null
         : null}
       <div className="hero-content flex-col lg:flex-row-reverse">
