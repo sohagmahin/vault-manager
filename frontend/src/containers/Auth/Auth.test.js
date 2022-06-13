52efd3a64c24ba7e3c8584a1ad8e4123c73b6da8
sohagmahin <sohagmahin@gmail.com>
2022-06-13 13:49:06 +0600

    test: auth component incomplete

---
+++ frontend/src/containers/Auth/Auth.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Auth from "./Auth";
+
+/* ---!important--- */
+//Todo: Redux mock needed.
+
+describe("Auth Testing", () => {
+  it("Have Header component", () => {
+    // render(<Auth />);
+    // const text = screen.getByText("Vault Manager");
+    // expect(text).toBeInTheDocument();
+  });
+  test("Have all auth texts", () => {});
+});
