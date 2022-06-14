3233e2ec9c86faf4c7619a2af8994aa0c5935155
sohagmahin <sohagmahin@gmail.com>
2022-06-14 13:18:53 +0600

    render Wrapped with redux provider and test auth page

--- frontend/src/containers/Auth/Auth.test.js
+++ frontend/src/containers/Auth/Auth.test.js
@@ -1,14 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "../../store/reducers";
+import { render, screen, cleanup } from "@testing-library/react";
 import Auth from "./Auth";
+import userEvent from "@testing-library/user-event";
 
-/* ---!important--- */
-//Todo: Redux mock needed.
+function renderWithRedux(
+  component,
+  { initialState, store = createStore(reducers, initialState) } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+  };
+}
 
-describe("Auth Testing", () => {
-  it("Have Header component", () => {
-    // render(<Auth />);
-    // const text = screen.getByText("Vault Manager");
-    // expect(text).toBeInTheDocument();
+describe("authentication page", () => {
+  afterEach(cleanup);
+  it("Have login mode", async () => {
+    renderWithRedux(<Auth />);
+
+    expect(screen.getByText(/Forgot password/)).toBeInTheDocument();
+    expect(screen.getByText(/Password/)).toBeInTheDocument();
+    expect(screen.getByText(/Username/)).toBeInTheDocument();
+    expect(screen.queryByText(/Name/)).toBeNull();
+
+    expect(
+      await screen.findByRole("button", { name: "LOGIN" })
+    ).toBeInTheDocument();
   });
-  test("Have all auth texts", () => {});
+
+  it("Have Register mode", async () => {
+    renderWithRedux(<Auth />);
+    expect(screen.queryByText("Name")).toBeNull();
+    await userEvent.click(screen.getByText(/Do you want to register?/));
+    expect(screen.getByText("Name")).toBeInTheDocument();
+    expect(screen.getByText(/Do you want to login?/)).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: "REGISTER" })
+    ).toBeInTheDocument();
+  });
 });
