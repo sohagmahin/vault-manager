6fbaf8ac896c976c13c01ee1066551135cf32737
sohagmahin <sohagmahin@gmail.com>
2022-07-18 14:14:09 +0600

    footer disappear while scrolling down to the page.

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const Footer = () => {
-  return (
+  const [show, setShow] = useState(true);
+
+  const controlFooter = () => {
+    window.scrollY > 100 ? setShow(false) : setShow(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlFooter);
+
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return show ? (
     <footer className="footer footer-center fixed bottom-0 p-4 bg-base-300 text-base-content">
       <div>
         <p>Copyright © 2022 - All right reserved by vault manager</p>
       </div>
     </footer>
+  ) : (
+    <></>
   );
 };
 
