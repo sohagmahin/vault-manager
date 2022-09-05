42a8a42945f7054ca942d4b7d1815cd1ac020b8e
sohagmahin <sohagmahin@gmail.com>
2022-09-05 16:36:31 +0600

    refactore code

--- backend/helpers/connectDB.js
+++ backend/helpers/connectDB.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+const DB_URL = process.env.DB_URL || "http://localhost/db/vaults";
+
 const connectDB = () => {
   // connect mongoDB
   mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(DB_URL)
     .then(() => console.log("MongoDB connected successfully!"))
     .catch((err) => console.log(err));
 };
