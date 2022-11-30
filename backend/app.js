68766e99494dd2546bcc4fca81cc5dc42e8ab270
sohagmahin <sohagmahin@gmail.com>
2022-12-01 00:57:46 +0600

    add hardcoded baseURL

--- backend/app.js
+++ backend/app.js
@@ -15,35 +15,6 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  return next();
-});
-
 // routes
 app.use("/vault", vaultHandler);
 app.use("/user", userHandler);
--- frontend/src/services/NetworkService.js
+++ frontend/src/services/NetworkService.js
@@ -3,7 +3,8 @@ import store from "../store/index";
 import { openErrorModal } from "../store/actions/index";
 import { TOKEN } from "../constants/keys";
 
-const baseURL = process.env.REACT_APP_API_URL;
+// const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = "http://20.193.139.57";
 
 const instance = axios.create({
   // baseURL: "http://localhost:3001",
