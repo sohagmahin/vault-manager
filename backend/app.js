435e9b4e76ba2739a93bd3ef3f39e2c3547a0d01
sohagmahin <sohagmahin@gmail.com>
2022-12-01 00:35:58 +0600

    405 fixes

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,35 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
+});
+
 // routes
 app.use("/vault", vaultHandler);
 app.use("/user", userHandler);
@@ -37,5 +66,12 @@ app.use("/", (req, res) =>
     message: "Hello word",
   })
 );
+
+// app.all("*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Methods", "PUT, GET,POST");
+//   next();
+// });
 
 module.exports = app;
