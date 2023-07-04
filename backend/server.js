c2848bc041ad0c4aeba457fee33477d1e17a1446
sohagmahin <sohagmahin@gmail.com>
2023-07-05 02:12:08 +0600

    set port

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 const connectDB = require("./helpers/connectDB");
+const PORT = process.env.PORT || 3001;
 
 //connect to mongoDB
 connectDB();
 
 // start server
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on ${process.env.PORT} port`);
 });
